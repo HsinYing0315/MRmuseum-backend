534e7951c22fef5099fd3200e24d9e409aba7a15
Melody Wu <110306066@g.nccu.edu.tw>
2024-04-12 15:38:15 +0800

    feat: add swagger

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flasgger import Swagger
 
 from .config import config
 
@@ -7,6 +8,7 @@ db = SQLAlchemy()
 
 def create_app(config_mode):
     app = Flask(__name__)
+    Swagger(app)
     app.config.from_object(config[config_mode])
 
     db.init_app(app)
