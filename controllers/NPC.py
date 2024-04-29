adc0c0a6488a39f717d5ede68a4f324c62590685
Melody Wu <110306066@g.nccu.edu.tw>
2024-04-30 00:20:57 +0800

    feat(NPC): init

---
+++ controllers/NPC.py
@@ -0,0 +1,14 @@
+from flask import Blueprint
+from ..services.NPC import meet_NPC, interact_NPC 
+
+NPC_blueprint = Blueprint('NPC_blueprint', __name__)
+   
+@NPC_blueprint.route('/<NPC_id>', methods=['GET'])
+def meet_NPC_controller():
+    
+    return meet_NPC()
+
+@NPC_blueprint.route('/<NPC_id>', methods=['GET'])
+def interact_NPC_controller():
+    
+    return interact_NPC()
---
+++ services/NPC.py
@@ -0,0 +1,15 @@
+from flask import request, jsonify
+import uuid
+
+from .. import db
+from ..models.NPC import NPC
+
+def meet_NPC(exhibitID):
+    response = NPC.query.get(exhibitID=exhibitID).toDict()
+    
+    return jsonify(response)
+
+def interact_NPC():
+    request_form = request.form.to_dict()
+
+    return 
