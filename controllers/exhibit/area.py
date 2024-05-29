c48287bd1f875d0a6cc994f86af28fa6fba41081
Melody Wu <110306066@g.nccu.edu.tw>
2024-05-30 02:13:35 +0800

    fix(area): return only basic information

--- controllers/exhibit/area.py
+++ controllers/exhibit/area.py
@@ -3,6 +3,6 @@ from services.exhibit.area import get_area
 
 area_blueprint = Blueprint('area_blueprint', __name__)
 
-@area_blueprint.route('/<location>', methods=['GET'])
-def get_area_controller(location):
-    return get_area(location)
+@area_blueprint.route('/<name>', methods=['GET'])
+def get_area_controller(name):
+    return get_area(name)
--- services/exhibit/area.py
+++ services/exhibit/area.py
@@ -2,6 +2,14 @@ from flask import jsonify
 
 from models.exhibit.area import Area
 
-def get_area(location):
-    response = Area.query.get(location).toDict()
-    return jsonify(response)
+def get_area(name):
+    if (Area.query.filter_by(name=name).first() is None):
+        return jsonify({})
+    response = Area.query.filter_by(name=name).first().toDict()
+    
+    area = jsonify({
+        'name': response['name'],
+        'introduction': response['introduction'],
+    })
+
+    return area
