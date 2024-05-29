3e40188c7622b4835d58718e0c25602c34825fa9
Melody Wu <110306066@g.nccu.edu.tw>
2024-05-30 02:16:32 +0800

    fix(time): return only basic information

--- controllers/exhibit/time.py
+++ controllers/exhibit/time.py
@@ -3,7 +3,7 @@ from services.exhibit.time import get_time
 
 time_blueprint = Blueprint('time_blueprint', __name__)
 
-@time_blueprint.route('/<area_id>/<location>', methods=['GET'])
-def get_time_controller(area_id):
+@time_blueprint.route('/<area_id>/<name>', methods=['GET'])
+def get_time_controller(area_id, name):
     
-    return get_time(area_id)
+    return get_time(area_id, name)
--- services/exhibit/time.py
+++ services/exhibit/time.py
@@ -2,10 +2,14 @@ from flask import jsonify
 
 from models.exhibit.time import Time
 
-def get_time(area_id, location):
-    times = Time.query.filter_by(areaID=area_id, location=location).all()
-    if (times is None):
-        return jsonify([])
-    response = []
-    for time in times: response.append(time.toDict())
-    return jsonify(response)
+def get_time(area_id, name):
+    response = Time.query.filter_by(areaID=area_id, name=name).first().toDict()
+    if (response is None):
+        return jsonify({})
+    
+    time = jsonify({
+        'name': response['name'],
+        'introduction': response['introduction'],
+    })
+    
+    return time
