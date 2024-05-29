a6fb13187e9711e376bdcfb31a18cfe13450906b
Melody Wu <110306066@g.nccu.edu.tw>
2024-05-30 01:36:25 +0800

    feat: use flask migrate

--- __init__.py
+++ __init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flasgger import Swagger
+
+import sys, os
+current_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_path)
 
 # from config import config
 
@@ -9,9 +14,10 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     Swagger(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://melody:nccu306@postgres:5432/graduation'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://melody:nccu306@localhost:5432/graduation'
 
     db.init_app(app)
+    Migrate(app, db)
     with app.app_context():
         db.create_all()
 
