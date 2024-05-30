abd3863a1ad15ad042cac65a1715777fcda77f92
Melody Wu <110306066@g.nccu.edu.tw>
2024-05-30 10:17:41 +0800

    feat(exhibit): list all in time

--- controllers/exhibit/exhibit.py
+++ controllers/exhibit/exhibit.py
@@ -1,7 +1,11 @@
 from flask import Blueprint
-from services.exhibit.exhibit import get_exhibit
+from services.exhibit.exhibit import get_exhibit, list_all_exhibits_by_time
 
 exhibit_blueprint = Blueprint('exhibit_blueprint', __name__)
+
+@exhibit_blueprint.route('/<time_id>', methods=['GET'])
+def get_all_exhibits_controller(time_id):
+    return list_all_exhibits_by_time(time_id)
 
 @exhibit_blueprint.route('/<exhibit_id>', methods=['GET'])
 def get_exhibit_controller(exhibit_id):
