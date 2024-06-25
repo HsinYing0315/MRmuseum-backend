bb6bd6932ebd1c7a76e4b1c7b87ba6e966684614
Melody Wu <110306066@g.nccu.edu.tw>
2024-06-26 02:25:44 +0800

    fix: add missing request

--- controllers/questionnaire.py
+++ controllers/questionnaire.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-from services.questionnaire import create_Questionnaire
+from services.questionnaire import create_Questionnaire, QuestionnaireSchema
 
 questionnaire_router = APIRouter(prefix='/questionnaire', tags=['questionnaire'])
 
 @questionnaire_router.post('/add')
-def add_questionnaire_controller():
+def add_questionnaire_controller(questionnaire: QuestionnaireSchema):
     
-    return create_Questionnaire()
+    return create_Questionnaire(questionnaire)
--- controllers/visitor.py
+++ controllers/visitor.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-from services.visitor import create_visitor 
+from services.visitor import create_visitor, VisitorSchema
 
 visitor_router = APIRouter(prefix='/visitor', tags=['visitor'])
     
 @visitor_router.post('/add')
-def add_visitor_controller():
+def add_visitor_controller(visitor: VisitorSchema):
     
-    return create_visitor()
+    return create_visitor(visitor)
