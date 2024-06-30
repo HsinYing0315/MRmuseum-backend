ea355ac22760424f31a2f519fdabfc53cf0e1f0b
Melody Wu <110306066@g.nccu.edu.tw>
2024-06-30 19:53:11 +0800

    chore: use database env

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-DATABASE_URL = 'postgresql://melody:nccu306@localhost:5432/graduation'
+DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:5432/{os.getenv('POSTGRES_DB')}"
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
