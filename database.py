1bfaa70fa4e1c9ecc5cc2b6f1aaf43560a5fd09f
HsinYing0315 <wusinging0315@gmail.com>
2024-07-07 10:58:55 +0800

    fix: add session back

--- database.py
+++ database.py
@@ -10,4 +10,5 @@ DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+session = Session()
 Base = declarative_base()
