27a5441f8c2efb3adb6e8844b6547fc41ed8f952
Melody Wu <110306066@g.nccu.edu.tw>
2024-07-08 22:11:13 +0800

    fix: import models into metadata

--- __init__.py
+++ __init__.py
@@ -8,6 +8,15 @@ sys.path.append(current_path)
 def create_app():
     app = FastAPI()
 
+    from models.visitor import Visitor
+    from models.exhibit.exhibit import Exhibit
+    from models.exhibit.exhibitGroup import ExhibitGroup
+    from models.exhibit.time import Time
+    from models.exhibit.area import Area
+    from models.test.test import Test
+    from models.test.answer import Answer
+    from models.interaction import Interaction
+    from models.questionnaire import Questionnaire
     Base.metadata.create_all(bind=engine)
 
     return app
