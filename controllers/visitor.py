e18119cb944545763583d04d4f31746ff4914b86
Melody Wu <110306066@g.nccu.edu.tw>
2024-07-09 11:27:48 +0800

    feat: get all visitors' data

--- controllers/visitor.py
+++ controllers/visitor.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
-from services.visitor import create_visitor, VisitorSchema
+from services.visitor import create_visitor, get_visitors, VisitorSchema
 
 visitor_router = APIRouter(prefix='/visitor', tags=['visitor'])
     
+
+@visitor_router.get('')
+def get_visitors_controller():
+    
+    return get_visitors()
+
 @visitor_router.post('/add')
 def add_visitor_controller(visitor: VisitorSchema):
     
--- services/visitor.py
+++ services/visitor.py
@@ -5,6 +5,16 @@ import uuid
 from database import session, commit
 from models.visitor import Visitor
 
+def get_visitors():
+    visitors = session.query(Visitor).all()
+    if (visitors is None):
+        return JSONResponse(status_code=404, content={"message": "No visitors found"})
+    response = []
+    for visitor in visitors: 
+        visitor = visitor.toDict()
+        response.append(visitor)
+    return JSONResponse(content=response)
+
 class VisitorSchema(BaseModel):
     age: int
     count: str
